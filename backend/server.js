4652f3b42cc6c24049519d63d715b63a7d722dc4
Jihoon Choi <cjihoon1234@gmail.com>
2024-02-24 00:40:56 -0800

    Created express api

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const { chats } = require("./data/data");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("API is Running");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get('/api/chat/:id', (req, res) => {
+  const singleChat = chats.find(c => c._id === req.params.id);
+  res.send(singleChat);
 })
 
-app.get()
+const PORT = process.env.PORT || 3000
 
-app.listen(3000, console.log("Server started on PORT 3000"));
+
+app.listen(3000, console.log(`Server started on PORT ${PORT}`));
