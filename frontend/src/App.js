cf1bd4f27ee6e36e7825d54495c9ccc76a8f1a88
Jihoon Choi <cjihoon1234@gmail.com>
2024-03-07 22:33:18 -0800

    enabled interaction between frontend and backend using proxy and axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import { Button } from '@chakra-ui/react'
+import { Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import ChatPage from "./Pages/ChatPage";
 
 function App() {
   return (
     <div className="App">
-      <Button colorScheme='blue'>Button</Button>
+      <Route exact path="/" component={ Homepage } />
+      <Route path="/chats" component={ ChatPage } />
     </div>
   );
 }
