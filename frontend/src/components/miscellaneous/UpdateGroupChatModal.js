235310362b5f0590d8a0d1e2e823b7d68ac482b0
Jihoon Choi <cjihoon1234@gmail.com>
2024-03-31 03:35:49 -0700

    Worked on UI of chatbox

--- frontend/src/components/miscellaneous/UpdateGroupChatModal.js
+++ frontend/src/components/miscellaneous/UpdateGroupChatModal.js
@@ -23,7 +23,7 @@ import { ChatState } from "../../Context/ChatProvider";
 import UserBadgeItem from "../UserAvatar/UserBadgeItem";
 import UserListItem from "../UserAvatar/UserListItem";
 
-const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain }) => {
+const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain, fetchMessages }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [groupChatName, setGroupChatName] = useState();
   const [search, setSearch] = useState("");
@@ -120,6 +120,7 @@ const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain }) => {
 
       user1._id === user._id ? setSelectedChat() : setSelectedChat(data);
       setFetchAgain(!fetchAgain);
+      fetchMessages();
       setLoading(false);
     } catch (error) {
       toast({
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,6 @@ root.render(
         <App />
       </ChakraProvider>
     </ChatProvider>
-  </BrowserRouter>
+  </BrowserRouter>,
 );
 
