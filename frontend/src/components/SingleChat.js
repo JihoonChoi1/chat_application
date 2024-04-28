438c92daa9fc47e72ddba3507af73680b7b36911
Jihoon Choi <cjihoon1234@gmail.com>
2024-04-28 16:33:11 -0700

    enabled real-time functionality by using socket.io

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -10,12 +10,30 @@ import { Spinner, FormControl } from "@chakra-ui/react";
 import ScrollableChat from "./ScrollableChat";
 import axios from 'axios';
 import './styles.css';
+import io from 'socket.io-client';
+import Lottie from 'react-lottie';
+import animationData from "../animations/typing.json";
 
+
+const ENDPOINT = "http://localhost:3000";
+var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(false);
   const [newMessage, setNewMessage] = useState();
+  const [socketConnected, setSocketConnected] = useState(false);
+  const [typing, setTyping] = useState(false);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    }
+  };
 
   const toast = useToast();
   const { user, selectedChat, setSelectedChat } = ChatState();
@@ -39,6 +57,8 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
 
       setMessages(data);
       setLoading(false);
+
+      socket.emit('join chat', selectedChat._id);
     } catch (error) {
       toast({
         title: "Error Occured!",
@@ -52,11 +72,32 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
   }
 
   useEffect(() => {
+    socket = io(ENDPOINT);
+    socket.emit("setup", user);
+    socket.on('connected', () => setSocketConnected(true));
+    socket.on('typing', () => setIsTyping(true));
+    socket.on('stop typing', () => setIsTyping(false));
+  }, []);
+
+  useEffect(() => {
     fetchMessages();
+
+    selectedChatCompare = selectedChat;
   }, [selectedChat]);
+
+  useEffect(() => {
+    socket.on('message received', (newMessageReceived) => {
+      if (!selectedChatCompare || selectedChatCompare._id !== newMessageReceived.chat._id) {
+        // give notification
+      } else {
+        setMessages([...messages, newMessageReceived]);
+      }
+    })
+  })
 
   const sendMessage = async (event) => {
     if (event.key === "Enter" && newMessage) {
+      socket.emit('stop typing', selectedChat._id);
       try {
         const config = {
           headers: {
@@ -74,7 +115,8 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
         );
         
         console.log(data);
-
+        
+        socket.emit('new message', data);
         setMessages([...messages, data]);
       } catch (error) {
         toast({
@@ -89,8 +131,26 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
     }
   }
 
+
+
   const typingHandler = (e) => {
     setNewMessage(e.target.value);
+    if (!socketConnected) return;
+
+    if (!typing) {
+      setTyping(true);
+      socket.emit('typing', selectedChat._id);
+    }
+    let lastTypingTime = new Date().getTime();
+    var timerLength = 3000;
+    setTimeout(() => {
+      var timeNow = new Date().getTime();
+      var timeDiff = timeNow - lastTypingTime;
+      if (timeDiff >= timerLength && typing) {
+        socket.emit('stop typing', selectedChat._id);
+        setTyping(false);
+      }
+    }, timerLength);
   }
 
   return (
@@ -149,6 +209,14 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
                 </div>
             )}
             <FormControl onKeyDown={sendMessage} isRequired mt={3}>
+              {console.log(isTyping)}
+              {isTyping ? <div>
+                <Lottie
+                  options={defaultOptions}
+                  width={70}
+                  style={{marginBottom: 15, marginLeft: 0}}
+                />
+              </div> : <></>}
               <Input
                 variant="filled"
                 bg="E0E0E0"
